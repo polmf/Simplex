69276be3f26d59e252886f5c1b6d10b7bab986bd
polmf <131487213+polmf@users.noreply.github.com>
2024-03-06 16:58:22 +0100

    inicial

--- simplex.py
+++ simplex.py
@@ -3,6 +3,9 @@ class Simplex:
 
     def __init__(self, c, b, A) -> None:
         
+        self.funcio_objectiu = c
+        self.b = b
+        self.restriccions = A
         pass
 
     def fase_1(self):
